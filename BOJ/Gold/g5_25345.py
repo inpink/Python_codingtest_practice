efee07b8514f4ddaeeef34865e5e3a6a7f0a65a0
inpink <inpink@kakao.com>
2022-08-08 22:38:25 +0900

    Semi-Game Cup 3 A번 이항정리 업데이트

--- BOJ/Gold/g5_25345.py
+++ BOJ/Gold/g5_25345.py
@@ -1,3 +1,5 @@
+#1번 코드. 이항정리를 사용하지 않아 6배 정도 덜 효율적이었다.
+'''
 import sys
 
 input = sys.stdin.readline
@@ -33,6 +35,38 @@ A = list(map(int, input().split()))
 # 경우의 수 문제. k개의 겹치지 않는 케이스를 뽑아서, 가장 큰 것을 어디에 세우느냐
 
 ans = c(N, K) * games(K)
+
+print(ans % (10 ** 9 + 7))
+'''
+
+#2번 코드. 이항정리를 사용했더니 6배 효율적인 코드가 완성됐다.
+import sys
+
+input = sys.stdin.readline
+
+
+def c(n, k):
+    if k == 0 or n == k:  # 5c5나 5c0같은 경우 계산안하고 1
+        return 1
+    elif k == 1 or (n - k) == 1:  # 5c1나 5c4 같은 경우 계산안하고 5
+        return n
+    else:
+        if (n - k) < k:  # 5c3같은 경우 5c2로 하면 더 효율적이다.
+            k = n - k
+        a = 1  # 분자
+        b = 1  # 분모
+        for i in range(k):
+            a *= (n - i)
+            b *= (i + 1)
+        return (a // b)  # nCk 값을 return
+
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+# 경우의 수 문제. k개의 겹치지 않는 케이스를 뽑아서, 가장 큰 것을 어디에 세우느냐
+
+ans = c(N, K) * 2 ** (K - 1)
 
 print(ans % (10 ** 9 + 7))
 
