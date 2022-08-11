21ffa6badfc8d8dd25c3f283897a08e391d38805
inpink <inpink@kakao.com>
2022-08-11 22:55:52 +0900

    백준 1920

---
+++ BOJ/Silver/s4_1920.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+ns = set(map(int, input().split()))
+
+m=int(input())
+ms = list(map(int, input().split()))
+
+for i in ms:
+    if i in ns:
+        print(1)
+    else:
+        print(0)
