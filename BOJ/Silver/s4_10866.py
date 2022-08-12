fb333b4266278ca8f494f6d7a6d19f16dcec7a3d
inpink <inpink@kakao.com>
2022-08-12 11:51:44 +0900

    백준 10866 덱(dequeue)

---
+++ BOJ/Silver/s4_10866.py
@@ -0,0 +1,52 @@
+import sys
+
+input = sys.stdin.readline
+
+from collections import deque
+
+q = deque()
+
+n = int(input())
+
+for i in range(n):  # n번 반복
+    n = list(input().split())
+    if n[0] == 'push_front':  # push_front인 경우
+        q.appendleft(n[1])
+
+    elif n[0] == 'push_back':
+        q.append(n[1])
+
+    elif n[0] == 'pop_front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            help = q.popleft()
+            print(help)
+
+    elif n[0] == 'pop_back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            help = q.pop()
+            print(help)
+
+    elif n[0] == 'size':
+        print(len(q))
+
+    elif n[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif n[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+
+    elif n[0] == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
